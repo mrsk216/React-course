5712648bae654af39bb9d279dce1b0562442939f
mrsk216 <freelancersk216@gmail.com>
2025-05-11 22:32:47 +0600

    Class 64 (Form, toggle)

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    term: '',
+  });
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [term, setTerm] = useState('');
+  
+  function handleFormData(e) {
+    e.preventDefault();
+    
+    setFormData({
+      name: name,
+      email: email,
+      password: password,
+      term: term
+    });
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Registration Form</h1>
+      <form onSubmit={handleFormData}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name='name' onChange={e => setName(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name='email'  onChange={e => setEmail(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name='password'  onChange={e => setPassword(e.target.value)}/>
+        </div>
+        <div>
+          <input type="checkbox" name="term"  onChange={e => setTerm(e.target.value)}/>
+          <label htmlFor="term">I agree with term and conditions</label>
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+      <h1>Registered User Info</h1>
+
+      {formData.name != '' ? (
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>Term</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{ formData.name }</td>
+              <td>{formData.email}</td>
+              <td>{formData.password}</td>
+              <td>{formData.term == 'on' ? 'Agree' : ''}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : ''}
+      
     </>
   )
 }
