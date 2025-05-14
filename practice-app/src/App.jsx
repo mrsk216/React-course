dc06aefbf17392a1a7eac8b1b139da0574606e52
mrsk216 <freelancersk216@gmail.com>
2025-05-14 23:31:24 +0600

    class 67 (first react project)

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -1,41 +1,29 @@
-import './App.css';
-import Feature from './Component/Features';
+import Header from './Component/Header'
 
-function App({ name }) {
-  const data = [
-    {
-      icon: 'https://frontdeskvisitor.com/assets/images/img/uploads/employee-management.png',
-      title: 'Tracking Visitors in Real-time',
-      description: 'Real-time visitor tracking has become an essential tool for businesses, event organizers, and service providers seeking to understand and optimize customer interactions. By monitoring visitor movements and behavior as they happen, organizations gain a precise view of engagement levels, helping them assess which areas or features draw the most attention. This real-time insight enables proactive decision-making: adjustments can be made instantly to streamline visitor flow, reduce congestion, and improve access to popular features or displays.'
-    },
-    {
-      icon: 'https://frontdeskvisitor.com/assets/images/img/uploads/clock.png',
-      title: 'Pre Booking Visitor',
-      description: "Pre-booking a visitor refers to the process of scheduling and reserving an appointment, ticket, or access for a visitor in advance, often through an online platform or physical booking system. This concept is widely applied in various industries, including hospitality, events, tourism, and even office management, ensuring that visitor's experiences are smooth, organized, and personalized.",
-      social: [
-        {
-          site: 'Facebook'
-        }
-      ]
-    },
-    {
-      icon: 'https://frontdeskvisitor.com/assets/images/img/uploads/keys.png',
-      title: 'Key Management',
-      description: 'Key management refers to the process of managing cryptographic keys used to protect sensitive data and ensure secure communication within a system. It involves various stages, including key generation, storage, distribution, rotation, and eventual destruction. Proper key management is essential for maintaining the confidentiality, integrity, and authenticity of encrypted data. Without effective key management, even the most advanced encryption systems can be compromised.'
-    },
-    {
-      icon: 'https://frontdeskvisitor.com/assets/images/img/uploads/delivery.png',
-      title: 'Handling Deliveries',
-      description: 'Handling deliveries is a crucial part of logistics and supply chain management, ensuring that products are safely and efficiently delivered to customers or businesses. It involves a series of steps from receiving an order to ensuring the timely and accurate arrival of goods at their intended destination. The process typically includes order verification, packaging, labeling, and choosing the appropriate transportation method based on factors like distance, urgency, and cost.'
-    },
-  ];
+function App() {
   
   return (
     <>
-      <h1>Features</h1>
-      <Feature farid={data} name={name} />
+      <Header />
+      <section className="home-hero">
+        <div className="container h-100">
+          <div className="row justify-content-center align-items-center h-100">
+            <div className="col-12 col-lg-8 col-xl-6 text-center">
+              <h1 className="display-5 fw-bold">Shekh Md Mohsin</h1>
+              <p className="mt-3 mb-4">
+                A Result-Oriented Web Developer building and managing Websites
+                and Web Applications that leads to the success of the overall
+                product
+              </p>
+              <a href="" className="btn btn-custom">
+                Hire Me
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
 export default App
--- practice-app/src/main.jsx
+++ practice-app/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './../public/assets/css/style.css'
 import App from './App'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App name="Akash"/>
+    <App/>
   </StrictMode>,
 )
