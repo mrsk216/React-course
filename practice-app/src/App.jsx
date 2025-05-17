ebf1932b2177ed3dc74c5cdc5785101674b4c9e0
mrsk216 <freelancersk216@gmail.com>
2025-05-17 22:54:57 +0600

    class 68 (tailwindcss installation, event propagation, react-icons installation+ use)

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -1,23 +1,27 @@
 import Header from './Component/Header'
+import { FaHireAHelper, FaUserGraduate } from "react-icons/fa"
 
 function App() {
   
   return (
     <>
       <Header />
-      <section className="home-hero">
+      <section className="home-hero" onClick={() => console.log('home hero has been clicked')}>
         <div className="container h-100">
-          <div className="row justify-content-center align-items-center h-100">
-            <div className="col-12 col-lg-8 col-xl-6 text-center">
-              <h1 className="display-5 fw-bold">Shekh Md Mohsin</h1>
-              <p className="mt-3 mb-4">
+          <div className="row justify-content-center align-items-center h-100" onClick={() => console.log('row has been clicked')}>
+            <div className="col-12 col-lg-8 col-xl-6 d-flex flex-column justify-content-center align-items-center">
+              <h1 className="display-5 fw-bold text-green-600"><FaUserGraduate className='faUserGraduate' />Shekh Md Mohsin</h1>
+              <p className="text-center mt-3 mb-4" onClick={() => console.log('p has been clicked')}>
                 A Result-Oriented Web Developer building and managing Websites
                 and Web Applications that leads to the success of the overall
                 product
               </p>
-              <a href="" className="btn btn-custom">
-                Hire Me
-              </a>
+              <button className="btn btn-custom d-flex align-items-center gap-2" onClick={(e) => {
+                e.stopPropagation();
+                console.log('btn has been clicked');
+              }}>
+                <FaHireAHelper />  Hire Me
+              </button>
             </div>
           </div>
         </div>
--- practice-app/src/main.jsx
+++ practice-app/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './../public/assets/css/style.css'
 import App from './App'
